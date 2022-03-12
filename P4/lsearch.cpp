2a8c759b392cbdc308cdc4d6e63047ad98dfa2fe
deft <Davego@protonmail.com>
2022-03-13 02:55:26 +0800

    generic liner search

--- P4/lsearch.cpp
+++ P4/lsearch.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstring>
 using namespace std;
 
 int lsearch(int key, int array[], int size)
@@ -11,9 +12,22 @@ int lsearch(int key, int array[], int size)
   return -1;
 }
 
+void *Lsearch(void *key,void *base,int n,int elemSize)
+{
+  for(int i=0;i<n;i++) {
+    void *elemAddr = (char*)base + i*elemSize;//seduce this base to one character(1byte)
+    //only this can do pointer arithmatic,caompiler need know the "unit"
+    if(memcmp(key,elemAddr,elemSize)==0)
+      return elemAddr;
+  }
+  return NULL;
+}
+
 int main() {
   int array[] = {1,5,9,-4,0,76};
   int index = lsearch(76,array,sizeof(array)/sizeof(array[0]));
   cout << "index = " << index << endl;
+  int key = 76;
+  cout << *((int*)(Lsearch(&key,array,sizeof(array)/sizeof(array[0]),sizeof(int)))) << endl;
 }
 
