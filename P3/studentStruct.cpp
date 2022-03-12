83492df8783d5a9a44ce963eacadba5e010932c8
deft <Davego@protonmail.com>
2022-03-12 16:34:06 +0800

    type in memory

--- P3/studentStruct.cpp
+++ P3/studentStruct.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <string.h>
+
 using namespace std;
 
 struct student{
@@ -7,7 +9,25 @@ struct student{
     int numUnits;
 };
 
+student pupils[4];
+
+void swap(int *ap, int *bp) {
+    int temp = *ap;
+    *ap = *bp;
+    *bp = temp;
+}
+
 int main() {
-  
+  pupils[0].numUnits = 21;
+  pupils[2].name = strdup("Admam");
+  pupils[3].name = pupils[0].suid + 6;
+  strcpy(pupils[1].suid,"40415xx");
+  strcpy(pupils[3].name,"123456");
+  pupils[7].suid[11] = 'A';
+  int x = 7;
+  int y = 117;
+  swap(&x,&y);
+  cout << "x = " << x << endl;
+  cout << "y = " << y << endl;
 }
 
